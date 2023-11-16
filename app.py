1e0beafa3d8b9863679cea2dbf960ca9faea318b
jamazoz <69641292+jamazoz@users.noreply.github.com>
2023-11-16 06:51:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,31 +23,4 @@ with col2:
   if st.button('Presiona el Botón'):
     st.write('Gracias por presionar')
 
-st.subheader("Uso de Botones")
-if st.button('Presiona el Botón'):
-  st.write('Gracias por presionar')
-else:
-  st.write('No has presionado aún')
-
-st.subheader("Selectbox")
-in_mod = st.selectbox(
-  "Selecciona la modalidad",
-  ("Audio","Visual","Háptico"),
-)
-if in_mod =="Audio":
-  set_mod = "Reproducir audio"
-elif in_mod == "Visual":
-  set_mod = "Reproducir video"
-elif in_mod == "Háptico":
-  set_mod = "Activar Vibración"
-st.write(" La acción es: ", set_mod)
-
-
-with st.sidebar:
-  st.subheader("Configura la modalidad")
-  mod_radio = st.radio(
-    "Escoge la modalidad a usar",
-    ("Visual", "Auditiva", "Háptica")
-  )
-
 st.write('Hecho por Julian Andres Mazo')
