80605f26bae0d5e98f751c9cd049009b88d1c709
jamazoz <69641292+jamazoz@users.noreply.github.com>
2023-11-16 07:19:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import os
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+import time
+import glob
+import paho.mqtt.client as paho
+import json
+from gtts import gTTS
+from googletrans import Translator
 import streamlit as st
-import paho.mqtt.client as mqtt
 from time import sleep
 from PIL import Image
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
 
 #MQTT Config
 MQTT_BROKER = os.getenv("broker.mqttdashboard.com")
@@ -9,6 +29,9 @@ MQTT_PORT = int(os.getenv("1883"))
 MQTT_TOPIC = "luzenvio"
 MQTT_USER = os.getenv("")
 MQTT_PW = os.getenv("")
+
+client1= paho.Client("GIT-HUB")
+client1.on_message = on_message
 
 
 st.title('Luces inteligentes con MQTT')
