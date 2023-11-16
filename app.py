54deef394f421447213fd52aa4bfefe0cb7627e0
jamazoz <69641292+jamazoz@users.noreply.github.com>
2023-11-16 07:24:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,9 @@ def on_message(client, userdata, message):
 
 
 #MQTT Config
-MQTT_BROKER = os.getenv("broker.mqttdashboard.com")
-MQTT_PORT = int(os.getenv("1883"))
+MQTT_BROKER = "broker.mqttdashboard.com"
+MQTT_PORT = 1883
 MQTT_TOPIC = "luzenvio"
-MQTT_USER = os.getenv("")
-MQTT_PW = os.getenv("")
 
 client1= paho.Client("GIT-HUB")
 client1.on_message = on_message
@@ -55,5 +53,51 @@ with col2:
   st.subheader("Apagar las luces")
   if st.button('Presiona el Botón para apagar'):
     st.write('Gracias por presionar')
+
+#Codigo copiado rec. texto 
+st.write("Toca el Botón y habla ")
+
+stt_button = Button(label=" Inicio ", width=200)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
+        client1.on_publish = on_publish                            
+        client1.connect(broker,port)  
+        message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
+        ret= client1.publish("voice_ctrl", message)
+
+    
+    try:
+        os.mkdir("temp")
+    except:
+        pass
 
 st.write('Hecho por Julian Andres Mazo')
